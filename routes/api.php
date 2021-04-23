6cd4610345739965977d659901ec65df3618de95
juan sebastian <sebastian_20lifefitt@hotmail.com>
2021-04-23 13:32:28 +0200

    rutas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ColegiosController;
+use App\Http\Controllers\ComunidadesController;
+use App\Http\Controllers\EventosController;
+use App\Http\Controllers\HospitalesController;
+use App\Http\Controllers\MunicipiosController;
+use App\Http\Controllers\ProvinciasController;
+use App\Http\Controllers\RestaurantesController;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\ViviendasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('Colegios', ColegiosController::class);
+Route::resource('Comunidades', ComunidadesController::class);
+Route::resource('Eventos', EventosController::class);
+Route::resource('Hospitales', HospitalesController::class);
+Route::resource('Municipios', MunicipiosController::class);
+Route::resource('Provincias', ProvinciasController::class);
+Route::resource('Restaurantes', RestaurantesController::class);
+Route::resource('Usuarios', UsuariosController::class);
+Route::resource('Viviendas', ViviendasController::class);
