972cb50161cbb5622103abab0483d3f29198f6f1
Pedro <pcastillotorio@gmail.com>
2021-04-29 12:19:45 +0200

    Añadida funcionalidad de login
    Ya se recogen datos de la db desde typescript para confirmar credenciales de usuario e iniciar sesion
    Las respuestas de los resources de la api ahora se devuelven en formato JSON

--- resources/frontend/angular/vida/src/app/componentes/login/Login.component.ts
+++ resources/frontend/angular/vida/src/app/componentes/login/Login.component.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../data.service';
 @Component({
     selector:'app-login',
     templateUrl:'./Login.component.html',
     styleUrls:['./Login.component.css'],
 })
 export class LoginComponent{
-    public iniciarSesion(nombre:string, contrasena:string):void {
+
+    constructor(private dataService: DataService) { }
+
+    public iniciarSesion(event:any, correo:string, contrasena:string):void {
         //llamar a la api y comprobar usuario valido
+        event.preventDefault()
+        var existe = 0;
+        var passOK = 0;
+        var isAdmin = 0;
+
+        console.log("Correo: " + correo + "\nPass: " +
+         contrasena);
+
+        //Comprobar si existe el correo
+        this.dataService.sendGetRequest('Usuarios/existeUsuario/' + correo).subscribe((data: any)=>{
+            existe = data;
+            console.log("Existe: " + existe);
+            //Si existe comprobamos contrasena
+            if(existe==1){
+                this.dataService.sendGetRequest('Usuarios/checkPassword/' + correo + "/" + contrasena).subscribe((data: any)=>{
+                    passOK = data;
+                    console.log("passOK: " + passOK);
+
+                    //Si la contrasena es correcta comprueba si es administrador
+                if(passOK==1){
+                    this.dataService.sendGetRequest('Usuarios/isAdmin/' + correo).subscribe((data: any)=>{
+                        isAdmin = data;
+                        console.log("isAdmin: " + isAdmin);
+
+                        if(isAdmin==1){
+                            console.log("Iniciaste sesión como Administrador")
+                        }
+    
+                        else{
+                            console.log("Iniciaste sesión como Usuario")
+                        }
+                    });
+
+                    
+                }
+
+                else{
+                    console.log("Correo y/o contraseña incorrecta")
+                }
+                });
+                
+            }
+
+            else{
+                console.log("Correo y/o contraseña incorrecta")
+            }
+                
+            });
+        
+        
+        
+
     }
 }
---
+++ resources/frontend/angular/vida/src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private REST_API_SERVER = "http://localhost/ProyectoComputacion2_3/public/api/";
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest(extension:string){
+    return this.httpClient.get(this.REST_API_SERVER + extension);
+  }
+}
